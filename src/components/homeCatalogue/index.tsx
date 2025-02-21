0b974c28ba6e16ff5207087efbeeb6aa88281a2f
Qi He <heqi37@gmail.com>
2025-02-21 18:54:36 +0800

    update CATALOGUE

--- src/components/homeCatalogue/index.tsx
+++ src/components/homeCatalogue/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { CATALOGUE } from './catalogue';
+import Link from '@docusaurus/Link';
 
 interface DocSlugProps {
   title: string;
-  slug: string;
+  docLink: string;
   link?: string;
 }
 
@@ -14,19 +15,36 @@ function Card({ catalogue }: { catalogue: { header: { title: string }; docs: Doc
       {catalogue.docs.map((doc, index) => {
         if (doc.link) {
           return (
-            <div key={index} className='truncate'>
-              <a target="_blank" className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700" title={doc.title} href={doc.link}>
+            <div key={index} className="truncate">
+              <a
+                target="_blank"
+                className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700"
+                title={doc.title}
+                href={doc.link}
+              >
                 {doc.title}
               </a>
             </div>
           );
         }
 
+        if (doc.docLink) {
+          return (
+            <div key={index} className="truncate">
+              <Link
+                className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700"
+                title={doc.title}
+                to={`/docs${doc.docLink}`}
+              >
+                {doc.title}
+              </Link>
+            </div>
+          );
+        }
+
         return (
-          <div key={index} className='truncate'>
-            <a className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700" title={doc.title} href={doc.slug}>
-              {doc.title}
-            </a>
+          <div key={index} className="truncate">
+            {doc.title}
           </div>
         );
       })}
