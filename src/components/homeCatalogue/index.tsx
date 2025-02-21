a02c94c4fd2a30538a0e06498599d2054fdd9101
Qi He <heqi37@gmail.com>
2025-02-21 14:30:26 +0800

    add link

--- src/components/homeCatalogue/index.tsx
+++ src/components/homeCatalogue/index.tsx
@@ -4,6 +4,7 @@ import { CATALOGUE } from './catalogue';
 interface DocSlugProps {
   title: string;
   slug: string;
+  link?: string;
 }
 
 function Card({ catalogue }: { catalogue: { header: { title: string }; docs: DocSlugProps[] } }) {
@@ -11,9 +12,19 @@ function Card({ catalogue }: { catalogue: { header: { title: string }; docs: Doc
     <div className="mb-2">
       <div className="mb-1 text-lg font-medium text-gray-900">{catalogue.header.title}</div>
       {catalogue.docs.map((doc, index) => {
+        if (doc.link) {
+          return (
+            <div key={index} className='truncate'>
+              <a target="_blank" className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700" title={doc.title} href={doc.link}>
+                {doc.title}
+              </a>
+            </div>
+          );
+        }
+
         return (
-          <div key={index}>
-            <a className="text-sm font-normal text-blue-600 hover:text-blue-500" href={doc.slug}>
+          <div key={index} className='truncate'>
+            <a className="text-sm font-normal text-blue-600 hover:text-blue-500 active:text-blue-700" title={doc.title} href={doc.slug}>
               {doc.title}
             </a>
           </div>
