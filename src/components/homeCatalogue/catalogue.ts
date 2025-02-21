3c66bbb452795a02b7c4a0c343580d04b8ffe012
Qi He <heqi37@gmail.com>
2025-02-21 14:24:21 +0800

    move CATALOGUE

---
+++ src/components/homeCatalogue/catalogue.ts
@@ -0,0 +1,41 @@
+export const CATALOGUE = [
+  {
+    header: { title: '可视化' },
+    docs: [
+      { title: '关于数据可视化', slug: '/viz/about-viz' },
+      { title: '可视化前的准备', slug: '/viz/prep-before-viz' },
+      { title: '利用三维面板可视化你的数据', slug: '/viz/use-3d-viz' },
+      { title: '面板', slug: '/viz/panel' },
+      { title: '三维面板', slug: '/viz/5-3d-panel' },
+      { title: '日志面板', slug: '/viz/log-panel' },
+      { title: '图表面板', slug: '/viz/plot-panel' },
+      { title: '图像面板', slug: '/viz/image-panel' },
+      { title: '布局', slug: '/viz/layout' },
+      { title: '在可视化界面中的一刻', slug: '/viz/create-moment-viz' },
+      { title: '帧率优化选项', slug: '/viz/frame-rate-optimization' },
+      { title: '发布面板', slug: '/viz/publish-panel' },
+      { title: '服务调用面板', slug: '/viz/service-panel' },
+      { title: '远程操控面板', slug: '/viz/teleop-panel' },
+      { title: '可视化选项', slug: '/viz/options' },
+    ],
+  },
+  {
+    header: { title: '设备' },
+    docs: [
+      { title: '关于设备', slug: '/device/device' },
+      { title: '添加设备', slug: '/device/create-device' },
+      { title: '管理设备', slug: '/device/manage-device' },
+      { title: '数采客户端配置', slug: '/device/device-collector' },
+      { title: '换机、升级、卸载流程指南', slug: '/device/change-device-guide' },
+      { title: '远程控制设备', slug: '/device/device-remote-control' },
+    ],
+  },
+  {
+    header: { title: '开发者' },
+    docs: [
+      { title: '概念', slug: '/category/core-concepts' },
+      { title: '命令行工具 coCLI - Beta',  slug: '/category/core-concepts' },
+      { title: '开放平台 OpenAPI',  slug: '/category/openapi' },
+    ],
+  },
+];
--- src/components/homeCatalogue/index.tsx
+++ src/components/homeCatalogue/index.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
+import { CATALOGUE } from './catalogue';
 
 interface DocSlugProps {
   title: string;
   slug: string;
 }
 
-const catalogue = [
-  {
-    header: { title: '可视化' },
-    docs: [
-      { title: '关于数据可视化', slug: '/viz/about-viz' },
-      { title: '可视化前的准备', slug: '/viz/prep-before-viz' },
-    ],
-  },
-  {
-    header: { title: '可视化' },
-    docs: [
-      { title: '关于数据可视化', slug: '/viz/about-viz' },
-      { title: '可视化前的准备', slug: '/viz/prep-before-viz' },
-    ],
-  },
-];
-
 function Card({ catalogue }: { catalogue: { header: { title: string }; docs: DocSlugProps[] } }) {
   return (
-    <div className='mb-2'>
+    <div className="mb-2">
       <div className="mb-1 text-lg font-medium text-gray-900">{catalogue.header.title}</div>
       {catalogue.docs.map((doc, index) => {
         return (
           <div key={index}>
-            <a className="text-base font-normal text-blue-600 hover:text-blue-500" href={doc.slug}>
+            <a className="text-sm font-normal text-blue-600 hover:text-blue-500" href={doc.slug}>
               {doc.title}
             </a>
           </div>
@@ -42,7 +26,7 @@ function Card({ catalogue }: { catalogue: { header: { title: string }; docs: Doc
 export default function HomeCatalogue() {
   return (
     <div className="grid grid-cols-4 gap-4">
-      {catalogue.map((item, index) => (
+      {CATALOGUE.map((item, index) => (
         <Card key={index} catalogue={item} />
       ))}
     </div>
