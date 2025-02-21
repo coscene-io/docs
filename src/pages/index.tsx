f11feb19cb479bca5b3113662cdb2a3709382c20
Qi He <heqi37@gmail.com>
2025-02-21 12:04:16 +0800

    add home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@theme/Layout';
+import React from 'react';
+import HomeCatalogue from '../components/homeCatalogue';
+
+export default function Home() {
+    return (
+        <Layout>
+            <HomeCatalogue />
+        </Layout>
+    );
+}
