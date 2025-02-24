334588b03d6e989a4bf96271ae0ba86dd4954a0e
Qi He <heqi37@gmail.com>
2025-02-24 11:17:53 +0800

    update width

--- src/components/homeCatalogue/index.tsx
+++ src/components/homeCatalogue/index.tsx
@@ -54,7 +54,7 @@ function Card({ catalogue }: { catalogue: { header: { title: string }; docs: Doc
 
 export default function HomeCatalogue() {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {CATALOGUE.map((item, index) => (
         <Card key={index} catalogue={item} />
       ))}
